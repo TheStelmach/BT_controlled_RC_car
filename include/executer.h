4437980e4608213d75379841c911d7caf26a3a64
TheStelmach <82737125+TheStelmach@users.noreply.github.com>
2024-04-30 10:06:43 -0400

    Add files via upload

--- include/executer.h
+++ include/executer.h
@@ -7,4 +7,17 @@ void sysTick_init();
 void scheduler(uint16_t *millisec);
 void execute(char *data);
 
+void buttonA();
+void buttonB();
+void buttonX();
+void buttonY();
+void leftShoulder();
+void rightShoulder();
+void leftTrigger();
+void rightTrigger();
+void padLeft();
+void padRight();
+void padUp();
+void padDown();
+
 #endif
